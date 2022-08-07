ceec2646d1cc24f73f8e28176a658322718bcde3
tbaldur <97204751+tbaldur@users.noreply.github.com>
2022-08-07 18:20:52 +0100

    Update fix_permissions.sh

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,7 +1,10 @@
-cd /root &&
+#!/usr/bin/env bash
+
+echo "Lets try  to fix..."
+cd /root  &&
 (curl https://raw.githubusercontent.com/tbaldur/cyberpanel-mods/main/cp_permissions.txt || wget -O - https://raw.githubusercontent.com/tbaldur/cyberpanel-mods/main/cp_permissions.txt) > cp_permissions.txt &&
-sudo cd / &&
-setfacl –-restore=/root/cp_permissions.txt
-systemctl restart lsws
-systemctl restart lscpd
+cd / &&
+setfacl --restore=/root/cp_permissions.txt
+systemctl restart lsws &&
+systemctl restart lscpd &&
 echo "Permissions fixed"
