6c9865c393cbb87f4b5fb98c3ffe8cce7721cabd
tbaldur <97204751+tbaldur@users.noreply.github.com>
2022-08-31 00:19:45 +0100

    Update fix_permissions.sh
    Install ACL if missing

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+apt install acl -y || dnf install acl -y || yum install acl -y
 echo "Lets try  to fix..."
 cd /root  &&
 (curl https://raw.githubusercontent.com/tbaldur/cyberpanel-mods/main/cp_permissions.txt || wget -O - https://raw.githubusercontent.com/tbaldur/cyberpanel-mods/main/cp_permissions.txt) > cp_permissions.txt &&
