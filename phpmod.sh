5382bda29ea1ce252e41e8eb08ce0d5852fd2cab
tbaldur <97204751+tbaldur@users.noreply.github.com>
2023-05-20 20:22:02 +0100

    Update phpmod.sh
    Fix: Re-run the change version after the install completes

--- phpmod.sh
+++ phpmod.sh
@@ -8,20 +8,24 @@ read -r -p "Choose one of the following php versions [71-72-73-74-80-81-82]: " I
 echo ""
 
 install_php_version() {
-  if ! (yum -y install lsphp$1* 2> /dev/null || apt-get install -y lsphp$1* 2> /dev/null || dnf install -y lsphp$1* 2> /dev/null); then
-    echo "Failed to install PHP $1. Package may not exist or package manager (yum/apt-get/dnf) may not be available."
+  if ! (yum -y install lsphp$1 2> /dev/null || apt-get install -y lsphp$1 2> /dev/null); then
+    echo "Failed to install PHP $1. Package may not exist or package manager (yum/apt-get) may not be available."
     exit 1
   fi
 }
 
+link_php_version() {
+  rm -f /usr/local/lscp/fcgi-bin/lsphp &&
+  ln -s /usr/local/lsws/lsphp$1/bin/lsphp /usr/local/lscp/fcgi-bin/lsphp
+  echo "Changed default version to $Input_Number"
+}
+
 change_php_version() {
   if [ -f /usr/local/lsws/lsphp$1/bin/lsphp ]; then
-    rm -f /usr/local/lscp/fcgi-bin/lsphp &&
-    ln -s /usr/local/lsws/lsphp$1/bin/lsphp /usr/local/lscp/fcgi-bin/lsphp
-    echo "Changed default version to $Input_Number"
+    link_php_version $1
   else
     echo "ERROR! Missing PHP $2. Installing..."
-    install_php_version $1
+    install_php_version $1 && link_php_version $1
   fi
 }
 
@@ -32,7 +36,7 @@ case "$Input_Number" in
   74) change_php_version 74 "7.4" ;;
   80) change_php_version 80 "8.0" ;;
   81) change_php_version 81 "8.1" ;;
-  82) change_php_version 82 "8.2" ;;
+  82) change_php_version 82 "8.1" ;;
   *)
     echo -e "Please write PHP version in the following format [71-72-73-74-80-81-82]\n"
     exit 1
