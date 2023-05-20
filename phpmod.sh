be6d23efdbc902cb78a0313b91d807d8802bb28a
tbaldur <97204751+tbaldur@users.noreply.github.com>
2023-05-20 20:15:32 +0100

    Update phpmod.sh
    Refactored script to avoid redundacy
    Installs lsphp with extentions if missing

--- phpmod.sh
+++ phpmod.sh
@@ -1,67 +1,40 @@
 #!/bin/bash
+
 echo "Default PHP version changer for cyberpanel apps"
 echo "Version chosen here will be used for phpmyadmin / snappymail"
 echo ""
-## read php version from input and configures it
-read -r -p "Choose one of the following php versions [71-72-73-74-80-81]: " Input_Number
+
+read -r -p "Choose one of the following php versions [71-72-73-74-80-81-82]: " Input_Number
 echo ""
+
+install_php_version() {
+  if ! (yum -y install lsphp$1* 2> /dev/null || apt-get install -y lsphp$1* 2> /dev/null || dnf install -y lsphp$1* 2> /dev/null); then
+    echo "Failed to install PHP $1. Package may not exist or package manager (yum/apt-get/dnf) may not be available."
+    exit 1
+  fi
+}
+
+change_php_version() {
+  if [ -f /usr/local/lsws/lsphp$1/bin/lsphp ]; then
+    rm -f /usr/local/lscp/fcgi-bin/lsphp &&
+    ln -s /usr/local/lsws/lsphp$1/bin/lsphp /usr/local/lscp/fcgi-bin/lsphp
+    echo "Changed default version to $Input_Number"
+  else
+    echo "ERROR! Missing PHP $2. Installing..."
+    install_php_version $1
+  fi
+}
+
 case "$Input_Number" in
-  71)
-  if [ -f /usr/local/lsws/lsphp71/bin/lsphp ]; then
-    rm -f /usr/local/lscp/fcgi-bin/lsphp &&
-    ln -s /usr/local/lsws/lsphp71/bin/lsphp /usr/local/lscp/fcgi-bin/lsphp
-    echo "Changed default version to $Input_Number"
-  else
-    echo "ERROR! Missing php 7.1? Check if /usr/local/lsws/lsphp71 exists."
-  fi
-  ;;
-  72)
-  if [ -f /usr/local/lsws/lsphp72/bin/lsphp ]; then
-    rm -f /usr/local/lscp/fcgi-bin/lsphp &&
-    ln -s /usr/local/lsws/lsphp72/bin/lsphp /usr/local/lscp/fcgi-bin/lsphp
-    echo "Changed default version to $Input_Number"
-  else
-    echo "ERROR! Missing php 7.2? Check if /usr/local/lsws/lsphp72 exists."
-  fi
-  ;;
-  73)
-  if [ -f /usr/local/lsws/lsphp73/bin/lsphp ]; then
-    rm -f /usr/local/lscp/fcgi-bin/lsphp &&
-    ln -s /usr/local/lsws/lsphp73/bin/lsphp /usr/local/lscp/fcgi-bin/lsphp
-    echo "Changed default version to $Input_Number"
-  else
-    echo "ERROR! Missing php 7.3? Check if /usr/local/lsws/lsphp73 exists."
-  fi
-  ;;
-  74)
-  if [ -f /usr/local/lsws/lsphp74/bin/lsphp ]; then
-    rm -f /usr/local/lscp/fcgi-bin/lsphp &&
-    ln -s /usr/local/lsws/lsphp74/bin/lsphp /usr/local/lscp/fcgi-bin/lsphp
-    echo "Changed default version to $Input_Number"
-  else
-    echo "ERROR! Missing php 7.4? Check if /usr/local/lsws/lsphp74 exists."
-  fi
-  ;;
-  80)
-  if [ -f /usr/local/lsws/lsphp80/bin/lsphp ]; then
-    rm -f /usr/local/lscp/fcgi-bin/lsphp &&
-    ln -s /usr/local/lsws/lsphp80/bin/lsphp /usr/local/lscp/fcgi-bin/lsphp
-    echo "Changed default version to $Input_Number"
-  else
-    echo "ERROR! Missing php 8.0? Check if /usr/local/lsws/lsphp80 exists."
-  fi
-  ;;
-  81)
-  if [ -f /usr/local/lsws/lsphp81/bin/lsphp ]; then
-    rm -f /usr/local/lscp/fcgi-bin/lsphp &&
-    ln -s /usr/local/lsws/lsphp81/bin/lsphp /usr/local/lscp/fcgi-bin/lsphp
-    echo "Changed default version to $Input_Number"
-  else
-    echo "ERROR! Missing php 8.1? Check if /usr/local/lsws/lsphp81 exists."
-  fi
-  ;;
+  71) change_php_version 71 "7.1" ;;
+  72) change_php_version 72 "7.2" ;;
+  73) change_php_version 73 "7.3" ;;
+  74) change_php_version 74 "7.4" ;;
+  80) change_php_version 80 "8.0" ;;
+  81) change_php_version 81 "8.1" ;;
+  82) change_php_version 82 "8.2" ;;
   *)
-  echo -e "  Please write php version in the following format [71-72-73-74-80-81]\n"
-  exit
+    echo -e "Please write PHP version in the following format [71-72-73-74-80-81-82]\n"
+    exit 1
   ;;
 esac
